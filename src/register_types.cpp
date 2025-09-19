86073044feaa1012d8b1e46730c1e9bc0b7bb11c
uig29852 <a22yo22io@gmail.com>
2025-09-19 09:30:43 +0200

    Added first summator class. Added traffic light class.

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -7,6 +7,9 @@
 
 #include "example_class.h"
 
+#include "summator.h"
+#include "traffic_light.h"
+
 using namespace godot;
 
 void initialize_gdextension_types(ModuleInitializationLevel p_level)
@@ -14,7 +17,8 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	GDREGISTER_CLASS(ExampleClass);
+	GDREGISTER_CLASS(Summator);
+	GDREGISTER_CLASS(TrafficLight);
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
